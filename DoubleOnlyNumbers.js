ae8a2ca33aff49ff9d6623da5473713517c7e6b2
YOGESH KURMI <31936428+10yogi@users.noreply.github.com>
2018-10-31 13:15:01 +0530

    Update DoubleOnlyNumbers.js

--- DoubleOnlyNumbers.js
+++ DoubleOnlyNumbers.js
@@ -1,6 +1,5 @@
 var arr = [1,2,3,'a','Techracers',5];
-
 var two_times_array = arr.map(function(ele){
-	return typeof ele ==='number' ? 2*ele: ele; 
+  return typeof ele ==='number' ? 2*ele: ele; 
 });
 console.log(two_times_array);
