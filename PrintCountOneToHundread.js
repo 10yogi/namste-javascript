cfe369fd65d19836647418acc2f689d27bf5c2d2
YOGESH KURMI <31936428+10yogi@users.noreply.github.com>
2018-10-31 13:16:02 +0530

    Update PrintCountOneToHundread.js

--- PrintCountOneToHundread.js
+++ PrintCountOneToHundread.js
@@ -1,8 +1,6 @@
-function PrintCount1to100()
-{
-	var output="";
-	for(var i = 1;i<=100;i=i+1)
-	{
+function PrintCount1to100(){
+  var output="";
+	for(var i = 1;i<=100;i=i+1){
 		output= output+i+" ";
 	}
 	console.log(output);
