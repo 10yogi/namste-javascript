d396c1bee99dc332a3a33860f76dd16a42cb8c8c
YOGESH KURMI <31936428+10yogi@users.noreply.github.com>
2018-10-31 13:13:31 +0530

    Update CustomJoin.js

--- CustomJoin.js
+++ CustomJoin.js
@@ -1,14 +1,12 @@
 var arr1 = [1,2,3,4,5,6,7];
 var arr2 = [8,9,10];
-
 var joint_array = [];
-
 for(index = 0;index<arr1.length; ++index){
-	joint_array.push(arr1[index]);
+  joint_array.push(arr1[index]);
 }
 for(index = 0;index<arr2.length;++index){
-	joint_array.push(arr2[index]);
+  joint_array.push(arr2[index]);
 }
 for(index = 0; index<joint_array.length;  ++index){
-	console.log(joint_array[i]);
+  console.log(joint_array[i]);
 }
