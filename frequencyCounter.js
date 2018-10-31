7bbf970500f8569f5fec323a725a4348b0eb5cc7
YOGESH KURMI <31936428+10yogi@users.noreply.github.com>
2018-10-31 13:22:00 +0530

    Update frequencyCounter.js

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -1,14 +1,13 @@
 frequency ={}; 
 var str = 'absaacdsie';
 for(i = 0;i<10;++i){
-	if(str[i] in frequency == false)
-		frequency[str[i]]=1;
-	else
-		frequency[str[i]] =frequency[str[i]]+1;
+  if(str[i] in frequency == false)
+    frequency[str[i]]=1;
+  else
+  frequency[str[i]] =frequency[str[i]]+1;
 }
-//console.log(frequency);
 for(c in frequency){
-	console.log(c+" "+frequency[c]);
+  console.log(c+" "+frequency[c]);
 }
 
 
